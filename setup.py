4c7536d6f2c5c8b6f1c76a8fb6af062f3cfebf6c
blcook223 <benjamin.l.cook@gmail.com>
2016-03-21 10:45:48 -0500

    add setup.py
    Added support for setup.py installation and other operations.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='dummy_data',
+      version='0.0.0',
+      description='Generate dummy data ',
+      url='http://github.com/blcook223/dummy_data',
+      author='Benjamin Cook',
+      author_email='benjamin.l.cook@gmail.com',
+      license='MIT',
+      packages=['dummy_data'],
+      zip_safe=False)
