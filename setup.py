7fb046199b8e39ab37df791d401425db506a9d36
blcook223 <benjamin.l.cook@gmail.com>
2016-04-06 23:28:15 -0500

    bump version to 0.0.1
    Bumped version number to 0.0.1.

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 
 setup(name='dummy_data',
-      version='0.0.0',
+      version='0.0.1',
       description='Generate dummy data ',
       long_description=readme(),
       keywords='dummy data json testing fake',
